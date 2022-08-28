eb9fe382161ee338d610bd8ecea7e38902fe37dc
Nicolò Zandarin <nikzanda@hotmail.it>
2022-08-28 09:54:28 +0200

    morgan

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import categoryRouter from './src/routers/category.routes';
 import filterRouter from './src/routers/filter.routes';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ apiRouter.use('/filters', filterRouter);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
 const frontendPath = __dirname + '/../frontend/dist';
 app.use('/', express.static(frontendPath));
