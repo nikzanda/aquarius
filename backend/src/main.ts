480ff90b8a23853ccf5fd59dff1740e3d37725f7
Nicolò Zandarin <nikzanda@LAPTOP-7OVF029P>
2022-10-09 19:06:54 +0200

    fix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
-const frontendPath = __dirname + '/../../frontend/dist';
+const frontendPath = __dirname + '/../../../frontend/dist';
 app.use('/', express.static(frontendPath));
 app.use('/api', apiRouter);
 
